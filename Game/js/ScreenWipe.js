937622a290c36a9ba25cc4ea56c5cd798dd5f6c3
Ian Rapoport <irapopor@ucsc.edu>
2017-05-27 15:05:57 -0700

    screen wipe?

--- Game/js/ScreenWipe.js
+++ Game/js/ScreenWipe.js
@@ -17,3 +17,41 @@ ScreenWipe.prototype.update = function(){
 
 };
 
+ScreenWipe.prototype.animIn = function(){
+    console.log("ANIMIN");
+    this.alpha = 1;
+    this.scale.setTo(4,6);
+    //1440, 810,
+    this.x = 700;
+    this.y = 400;
+
+    //Slide out of the way then hide
+    var tween = game.add.tween(this).to( { x: -1440 , y: -900 },1300,"Linear",true,0);
+    tween.onComplete.add(function(){         this.alpha = 0;       },this);    
+}
+
+ScreenWipe.prototype.animOut = function(nextFunc){
+    console.log("here");   
+    this.alpha = 1;
+    this.scale.setTo(4,6);
+    //1440, 810,
+    this.x = 1440 + 1600;
+    this.y = 810 + 1000;
+
+    //Slides over the view then calls the next state
+    var tween = game.add.tween(this).to( { x: 700, y: 400},1300,"Linear",true,0);
+    tween.onComplete.add(function(){         nextFunc();       },this);    
+}
+
+ScreenWipe.prototype.animOutComplex = function(nextFunc,xpos,ypos,scale){
+    console.log("here");   
+    this.alpha = 1;
+    this.scale.setTo(4+scale,6+scale);
+    //1440, 810,
+    this.x = xpos;
+    this.y = ypos;
+
+    //Slides over the view then calls the next state
+    var tween = game.add.tween(this).to( { x: xpos - 3040, y: ypos - 1410},1300,"Linear",true,0);
+    tween.onComplete.add(function(){         nextFunc();       },this);    
+}
